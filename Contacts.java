44fbd18d07a49efc2c5e1db7d5336b03cd6cd2d5
Sandra Davis <Sandra.Davis@godigit.com>
2024-07-25 16:34:18 +0530

    Changes in contacts class

--- Contacts.java
+++ Contacts.java
@@ -21,6 +21,70 @@ public class Contacts
         this.zip = zip;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String toString()
     {
         return firstname+" "+lastname+"\n"+address+"\n"+city+" "+state+"\n"+zip+"\n"+phonenumber+"\n"+email;
